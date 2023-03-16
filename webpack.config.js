6b5074fc5122a64a2ab3d6eb673ee5e595a353b3
volkankaraali <volkankaraali1@gmail.com>
2023-03-16 16:04:12 +0300

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8192,
+              limit: 1024000,
             },
           },
         ],
