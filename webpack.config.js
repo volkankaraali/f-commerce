a9791a084658d80f351a1c95b51b6933ab2f0a83
volkankaraali <volkankaraali1@gmail.com>
2023-03-16 15:54:18 +0300

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|png)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
